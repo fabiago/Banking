03d5514a63eaf4883a6957d0abda3054b212c4f8
fabiago <frestrepo7@gatech.edu>
2026-05-05 09:19:01 -0500

    added account class header file

--- include/account.h
+++ include/account.h
@@ -1,17 +1,28 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
 #include <iostream>
+#include <string>
 
 class Account {
-
-private:
-
-int accountNumer;
-std::string ownerName;
-int balance;
-std::string accountType;
+protected:
+    int accountNumber;
+    std::string ownerName;
+    double balance;
+    std::string accountType;
 
 public:
 
-Account();
-~Account();
+    // constructors
+    Account();
+    Account(int accNum, 
+        const std::string& name, 
+        double bal, 
+        const std::string& type);
 
+
+
+~Account();
 };
+
+#endif // ACCOUNT_H
