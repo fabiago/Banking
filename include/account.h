69710d4b540b2ac81cabacefd37984c56c992166
fabiago <frestrepo7@gatech.edu>
2026-05-05 09:24:42 -0500

    added memeber functions and memeber variables

--- include/account.h
+++ include/account.h
@@ -20,9 +20,23 @@ public:
         double bal, 
         const std::string& type);
 
+    // vitual destructor
+    virtual ~Account();
 
+    // getter functions
+    int getAccountNumer() const;
+    std::string getOwnerName() const;
+    double getBalance() const;
+    std::string getAccountType() const;
 
-~Account();
+    // setter functions
+    void setOwnerName(const std::string& name);
+    void setBalance(double bal);
+
+    // virtual functions
+    virtual void deposit(double amount);
+    virtual void withdraw(double amount);
+    virtual void display() const;
 };
 
 #endif // ACCOUNT_H
