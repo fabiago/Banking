7788531982a223cce85085175388de16478510d6
fabiago <frestrepo7@gatech.edu>
2026-05-05 10:03:54 -0500

    added checkingAccount child class

---
+++ include/checkingAccount.h
@@ -0,0 +1,30 @@
+#ifndef CHECKINGACCOUNT_H
+#define CHECKINGACCOUNT_H
+
+#include "account.h";
+
+class CheckingAccount: public Account{
+private:
+    double overdraftLimit;
+
+public:
+    
+    // constructors
+    CheckingAccount();
+    CheckingAccount(int accNum, 
+        const std::string& name, 
+        double bal, 
+        double overdraft);
+
+    // destructor
+    ~CheckingAccount();
+
+    // getter and setter
+    double getOverdraftLimit() const;
+    void setOverdraftLimit(double limit);
+
+    // ovr virtual functions
+    void withdraw(double amount) override;
+    void display() const override;
+};
+#endif
